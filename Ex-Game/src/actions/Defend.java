7fdd3e3163ef227124d8eee94952eaa90c91fede
Josh Bankston <s1c.pr0x@gmail.com>
2022-01-03 15:14:46 -0600

    Potentially not needed, but may provide cleaner structure

--- Ex-Game/src/actions/Defend.java
+++ Ex-Game/src/actions/Defend.java
@@ -4,7 +4,7 @@ import entities.ParentEntity;
 public class Defend extends ParentEntity{
 
     public int defend(int incDmg,int dex, int str) {
-    	
+    	//Calculate to always provide a 25% damage reduction bonus when 'defend' is selected
     	int damageReduction = 0;
         return damageReduction;
     }
